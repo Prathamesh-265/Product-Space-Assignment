d1fdf323f9460abf53eb6998880fb84785c1942c
Prathamesh-265 <prathameshkatre26@gmail.com>
2026-04-28 07:35:08 +0530

    remove mkdir causing render crash

--- Task-Master-Pro/server/src/index.ts
+++ Task-Master-Pro/server/src/index.ts
@@ -1,15 +1,8 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import * as schema from "./schema/index.js";
-import fs from "fs";
-import path from "path";
 
 const dbPath = process.env.DATABASE_PATH ?? "./taskflow.db";
-
-const dir = path.dirname(dbPath);
-if (!fs.existsSync(dir)) {
-  fs.mkdirSync(dir, { recursive: true });
-}
 
 const sqlite = new Database(dbPath);
 
