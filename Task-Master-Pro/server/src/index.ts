2a67bc5af81667f34708260b7fa9cd6f7e8171f0
Prathamesh-265 <prathameshkatre26@gmail.com>
2026-04-28 07:42:50 +0530

    auto create db tables

--- Task-Master-Pro/server/src/index.ts
+++ Task-Master-Pro/server/src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import app from "./app.js";
 import { seedDemoUser } from "./seedDemo.js";
+import { execSync } from "child_process";
 
 const port = Number(process.env.PORT ?? 3000);
 
@@ -11,6 +12,14 @@ process.on("uncaughtException", (err) => {
 process.on("unhandledRejection", (err) => {
   console.error("UNHANDLED PROMISE:", err);
 });
+
+
+try {
+  execSync("npx drizzle-kit push", { stdio: "inherit" });
+  console.log("Database schema created");
+} catch (e) {
+  console.error("DB setup failed:", e);
+}
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
